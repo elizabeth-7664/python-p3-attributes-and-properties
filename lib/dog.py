9c9d6e1d022c9f8b88d523ff4ac2d2338e2f3856
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-05-15 19:58:01 +0300

    Differentiating between variables, attributes, and propertie

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,30 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Pug"):
+        self.name = name
+        self.breed = breed
+
+    # -------- name property --------
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    name = property(get_name, set_name)
+
+    # -------- breed property --------
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='Guido', job='Sales'):
+        self.name = name
+        self.job = job
+
+    # -------- name --------
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()  # Make the name Title Case (e.g. "guido" -> "Guido")
+
+    name = property(get_name, set_name)
+
+    # -------- job --------
+    def get_job(self):
+        return self._job
+
+    def set_job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
+
+    job = property(get_job, set_job)
